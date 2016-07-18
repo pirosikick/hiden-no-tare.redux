76db1bd58b75f22027d604175770a375f9b597b1
Hiroyuki Anai <pirosikick@gmail.com>
2016-07-19 00:02:24 +0900

    Fixed lint

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,10 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
 
 export class App extends Component {
   constructor(props, context) {
     super(props, context);
     this.onSubmit = this.onSubmit.bind(this);
-  }
-
-  getMessage() {
-    return this.refs.message.value;
   }
 
   onSubmit(e) {
@@ -20,26 +16,35 @@ export class App extends Component {
     }
   }
 
+  getMessage() {
+    return this.refs.message.value;
+  }
+
   render() {
-    const {tasks, onAdd} = this.props;
+    const { tasks } = this.props;
     return (
       <div className="app">
         <h1>Hello! app!</h1>
         <form onSubmit={this.onSubmit}>
-          <input type="text" ref="message"/>
+          <input type="text" ref="message" />
           <button>Add</button>
         </form>
         <ul>
           {tasks.map(task => <li>{task}</li>)}
-          </ul>
-        </div>
+        </ul>
+      </div>
     );
   }
 }
 
+App.propTypes = {
+  tasks: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onAdd: PropTypes.func.isRequired,
+};
+
 const mapStateToProps = state => ({ tasks: state.tasks.toArray() });
 const mapDispatchToProps = dispatch => ({
-  onAdd: message => dispatch({ type: 'ADD_TASK', message })
+  onAdd: message => dispatch({ type: 'ADD_TASK', message }),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
